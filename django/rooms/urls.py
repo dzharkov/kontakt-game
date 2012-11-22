c1fdb504e576695c6cd6eedfbf199d065cea3cc9
Denis Zharkov <bintree@eblans.org>
2012-11-22 19:19:50 +0700

    Теперь показывается состояние комнаты и добавлен view для очистки контактов комнаты

--- django/rooms/urls.py
+++ django/rooms/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('rooms.views',
     # Examples:
     url(r'^(?P<id>\d+)$', 'room'),
+    url(r'^clear$', 'clear_room')
 )
--- django/rooms/views.py
+++ django/rooms/views.py
@@ -1,5 +1,21 @@
 from annoying.decorators import render_to
+from django.http import HttpResponse
+from games.models import *
+from django.contrib.auth import login
+from django.contrib.auth.models import User
+
+def clear_room(request):
+    Contact.objects.create_dummy_contacts_for_game()
+    return HttpResponse("cleared");
 
 @render_to('room/main.html')
 def room(request, id):
-    return {}
+    game = Game.objects.get_dummy_game()
+
+    user = User.objects.get(pk=id)
+    user.backend = 'django.contrib.auth.backends.ModelBackend'
+    login(request, user)
+
+    is_user_master = user == game.master
+
+    return { 'game' : game, 'is_user_master' : is_user_master }
