72159ae2e9367c58b45a0fa5c13cb62977d3556e
Denis Zharkov <bintree@eblans.org>
2012-11-29 16:27:00 +0700

    Классы для управлению комнатами (из торнадо), возможно излишние :)

--- rooms/models.py
+++ rooms/models.py
@@ -1,3 +1,15 @@
-from django.db import models
 
-# Create your models here.
+class Room(object):
+
+    def __init__(self):
+        self._users = dict()
+
+    def add_user(self, user):
+        self._users[user.id] = user
+
+    def remove_user(self, user):
+        del self._users[user.id]
+
+    @property
+    def users(self):
+        return self._users.values()
