7a04209a079b9c06752e7063c6d1ddab34e746f1
Denis Zharkov <bintree@eblans.org>
2012-11-29 16:19:49 +0700

    Во view, где отдается комната, записываю в редис, что пользователь входит в комнату

--- rooms/views.py
+++ rooms/views.py
@@ -1,9 +1,11 @@
+import redis
 from annoying.decorators import render_to
 from django.http import HttpResponse
-from games.models import *
 from games.managers import *
 from django.contrib.auth import login
 from django.contrib.auth.models import User
+
+from app import settings
 
 def clear_room(request):
     ContactManager.create_dummy_contacts_for_game()
@@ -16,4 +18,8 @@ def room(request, id):
     user.backend = 'django.contrib.auth.backends.ModelBackend'
     login(request, user)
 
-    return { }
+    redis_connection = redis.Redis(host=settings.REDIS_HOST, db=settings.REDIS_DB)
+
+    redis_connection.hset('room:1', request.session.session_key, id)
+
+    return { 'room_id' : 1 }
