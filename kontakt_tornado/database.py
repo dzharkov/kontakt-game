733cc780a828b8129d3698f20b9a6a172b3dc03f
Denis Zharkov <bintree@eblans.org>
2013-01-02 12:35:28 +0700

    Добавил соедиенение с БД, которое в отдельном треде будет выполнять поступающие запросы (пока только на модификацию без колбеков)

--- kontakt_tornado/database.py
+++ kontakt_tornado/database.py
@@ -2,8 +2,36 @@ import tornado.database
 import tornadoredis
 import redis
 
+import threading, Queue
+
 from app import settings
+
+class AsyncDatabaseModifier():
+    def __init__(self, pool_size=1):
+        self._threads = []
+        self._tasks = Queue.Queue()
+        # create threads
+        for i in xrange(pool_size):
+            t = DbWorker(self)
+            t.start()
+            self._threads.append(t)
+    def execute(self, query, *argv):
+        self._tasks.put([query, argv])
+
+class DbWorker(threading.Thread):
+    def __init__(self, cnt):
+        self._controller = cnt
+        self._db = tornado.database.Connection('localhost', settings.DATABASES['default']['NAME'], settings.DATABASES['default']['USER'], settings.DATABASES['default']['PASSWORD'])
+        super(DbWorker, self).__init__()
+    def run(self):
+        cnt = self._controller
+
+        while True:
+            task = cnt._tasks.get(True)
+            self._db.execute(task[0], *task[1])
 
 db = tornado.database.Connection('localhost', settings.DATABASES['default']['NAME'], settings.DATABASES['default']['USER'], settings.DATABASES['default']['PASSWORD'])
 redis_connection = redis.Redis(host=settings.REDIS_HOST, db=settings.REDIS_DB)
 redis_subscriptions = tornadoredis.Client(host=settings.REDIS_HOST, selected_db=settings.REDIS_DB)
+
+db_modifier = AsyncDatabaseModifier()
