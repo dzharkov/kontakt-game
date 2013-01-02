642ddc9c339514ea1d588a5e29aed422fab7dd68
Denis Zharkov <bintree@eblans.org>
2013-01-02 13:49:46 +0700

    Отделил view с авторизацией по user_id

--- rooms/urls.py
+++ rooms/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('rooms.views',
     # Examples:
-    url(r'^(?P<room_id>\d+)/(?P<user_id>\d+)$', 'room'),
+    url(r'^(?P<room_id>\d+)/(?P<user_id>\d+)$', 'authorize_user_and_redirect_to_room'),
+    url(r'^(?P<room_id>\d+)$', 'room'),
     url(r'^clear$', 'clear_room'),
     url(r'^create$', 'create'),
     url(r'^edit/(?P<id>\d+)$', 'edit'),
--- rooms/views.py
+++ rooms/views.py
@@ -24,17 +24,25 @@ def clear_room(request):
     return HttpResponse("cleared")
 
 @render_to('room/main.html')
-def room(request, room_id, user_id):
+@login_required
+def room(request, room_id):
 
-    user = User.objects.get(pk=user_id)
-    user.backend = 'django.contrib.auth.backends.ModelBackend'
-    login(request, user)
+    room = get_object_or_404(Room, pk=room_id)
+
+    user = request.user
 
     redis_connection = redis.Redis(host=settings.REDIS_HOST, db=settings.REDIS_DB)
 
     redis_connection.hset('room:' + str(room_id), request.session.session_key, user.id)
 
     return { 'room_id' : int(room_id) }
+
+def authorize_user_and_redirect_to_room(request, room_id, user_id):
+    user = User.objects.get(pk=user_id)
+    user.backend = 'django.contrib.auth.backends.ModelBackend'
+    login(request, user)
+
+    return HttpResponseRedirect(reverse('rooms.views.room', args=[room_id]))
 
 @render_to('room/create.html')
 @login_required
