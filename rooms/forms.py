403a9cd502d2e6f03c695364b060409efef0a6fc
Denis Zharkov <bintree@eblans.org>
2013-01-07 14:19:02 +0700

    Добавил в форму комнаты чекбокс приватности

--- rooms/forms.py
+++ rooms/forms.py
@@ -5,6 +5,7 @@ from models import Room
 class RoomForm(forms.ModelForm):
     class Meta:
         model = Room
-        fields = ('name', )
+        fields = ('name', 'is_private')
 
     name = forms.CharField(min_length=5, max_length=100, required=True, label=u"Название")
+    is_private = forms.BooleanField(label=u'Закрытая?', required=False)
