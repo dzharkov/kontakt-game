b07cce7f456cc325363957d78cda496aae31b1ad
typeinference <type.inference@gmail.com>
2013-01-13 00:02:56 +0700

    Добавил директиву ng-enter и отправку сообщения в чат по кнопке Enter

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,1 +1,9 @@
 var app = angular.module('myApp', []);
+
+app.directive('ngEnter', function() {
+    return function(scope, elm, attrs) {
+        elm.bind('keypress', function(e) {
+            if (e.charCode === 13) scope.$apply(attrs.ngEnter);
+        });
+    };
+}); 
