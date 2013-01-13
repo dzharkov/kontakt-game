fd8e7769ce5ac680c08d6472c317f1220f336294
Denis Zharkov <bintree@eblans.org>
2013-01-13 17:38:00 +0700

    Backend: фикстуры "прустота" -> "пустота"

--- rooms/views.py
+++ rooms/views.py
@@ -62,7 +62,7 @@ def clear_room(request):
         cursor.execute(u"INSERT INTO `rooms_room` VALUES (2,'Экзистенциальная Россия', 2, 0, 0)")
 
         cursor.execute(u"INSERT INTO `games_game` VALUES (1,2,'моделирование',2,1,'running')")
-        cursor.execute(u"INSERT INTO `games_game` VALUES (2,2,'прустота',8,2,'complete')")
+        cursor.execute(u"INSERT INTO `games_game` VALUES (2,2,'пустота',8,2,'complete')")
         cursor.execute(u"INSERT INTO `games_contact` VALUES (1,1,'2012-11-22 16:56:25',3,'мода','как сказала Коко Шанель, она выходит сама из себя',NULL,NULL,NULL,1,0),(2,1,'2012-11-22 17:09:04',4,'моделирование','Оно бывает имитационным, эволюционным, и изредка даже психологическим',NULL,NULL,NULL,1,0)")
     except Exception:
         transaction.rollback()
